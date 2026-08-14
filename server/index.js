f01213cb895bc31347972e2128ee91673431e128
bhagyashrisanap15 <bhagyashrisanap35@gmail.com>
2026-08-14 09:51:22 +0530

    create the folder and installtion

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    if (conn) {
+      console.log("mongoDB connected");
+    }
+  } catch (error) {
+    console.log("mongoDB connection error:", error);
+  }
+};
+
+app.get("/", (req,res) => {
+  res.json({
+    success: true,
+    message:"API is running...",
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
